7175f6f548baf624d33ef8e05d918848ba26a123
Chen0Zu <chen0zu@gmail.com>
2020-06-19 23:38:20 +0800

    linear algebra problem set 1.1

---
+++ LinearAlgebra/ProblemSet1_1.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+# 1
+A = np.array([[1, 3], [2, 6], [3, 9]])
+B = np.array([[1, 0], [0, 2], [0, 3]])
+C = np.array([[2, 0, 2], [0, 2, 2], [0, 2, 3]])
+rankA = np.linalg.matrix_rank(A)
+rankB = np.linalg.matrix_rank(B)
+rankC = np.linalg.matrix_rank(C)
+
+# 2
+v = np.array([4, 1]).reshape(-1, 1)
+w = np.array([-2, 2]).reshape(-1, 1)
+vplusw = v + w
+vmiusw = v - w
+
+# 3
+A = np.array([[1, 1], [1, -1]])
+B = np.array([[5, 1], [1, 5]])
+V = B @ np.linalg.inv(A)
+
+# 4
+A = np.array([[2, 1], [1, 2]])
+c = 3
+d = 1
+v = np.array([c, d]).reshape(-1, 1)
+B = A @ v
+
+# 6
+A = np.array([[1, -2, 1], [0, 1, -1]]).T
+c, d = 7, 6
+v = np.array([c, d]).reshape(-1, 1)
+o1 = np.sum(A @ v)
+
+# 7
+A = np.array([[2,0],[1,1]])
+B = np.array([[0,0,0,1,1,1,2,2,2],[0,1,2,0,1,2,0,1,2]])
+o = A @ B
